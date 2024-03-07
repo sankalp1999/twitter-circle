6808446689983bdfb4ef29dafc29710e9feb6473
sankalp1999 <sankalp.shubham99@gmail.com>
2024-03-07 19:52:00 +0530

    update the start script

--- start_server.js
+++ start_server.js
@@ -1,12 +1,15 @@
 const httpServer = require('http-server');
-const open = require('open');
 
 const port = 8080;
 
 const server = httpServer.createServer({
-  // server options
+    // server options
 });
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-  open(`http://localhost:${port}`);
+    console.log(`Server listening on port ${port}`);
+
+    // Dynamically import the 'open' package
+    import('open').then((open) => {
+        open.default(`http://localhost:${port}`);
+    }).catch(err => console.error('Failed to load the open module', err));
 });
